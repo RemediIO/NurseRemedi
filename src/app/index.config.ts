cf0cfc8406754d6ad40569194014810ea2e983c4
Orie Steele <orie@ohs.io>
2016-04-24 10:47:35 -0500

    tweak styles

--- src/app/index.config.ts
+++ src/app/index.config.ts
@@ -17,24 +17,24 @@ export function config(
 
 
     $mdThemingProvider
-        .theme('busybotsTheme')
+        .theme('remediTheme')
 
-        .primaryPalette('purple', {
-            'default': '700', // by default use shade 400 from the pink palette for primary intentions
+        .primaryPalette('blue', {
+            'default': '400', // by default use shade 400 from the pink palette for primary intentions
             'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
             'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
             'hue-3': 'A400' // use shade A100 for the <code>md-hue-3</code> class
         })
         // If you specify less than all of the keys, it will inherit from the
         // default shades
-        .accentPalette('pink', {
+        .accentPalette('purple', {
             'default': 'A700' // use shade 200 for default, and keep all other shades the same
         })
 
         .warnPalette('red')
         
         .backgroundPalette('grey', {
-            'default': '900',
+            'default': '200',
             'hue-1': '100',
             'hue-2': '200',
             'hue-3': '800',
@@ -42,6 +42,6 @@ export function config(
 
 
     $mdThemingProvider
-        .setDefaultTheme('busybotsTheme');
+        .setDefaultTheme('remediTheme');
 
 }
